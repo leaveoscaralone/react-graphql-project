48732b7621b31e1a9ba02aa14fee8f0304a7bbb9
marcoantonioghiani01 <marcoantonio.ghiani01@gmail.com>
2019-05-21 11:24:18 +0200

    upgrade to v2.2.3

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,9 @@ const $ = require('gulp-load-plugins')();
 // when generating the glob patterns array for gulp.src()
 function addDefSrcIgnore (srcArr) {
   return srcArr.concat([
-    '!**/node_modules{,/**}',
-    '!**/private{,/**}',
+    '!**/REMOVE{,/**}',
+    '!node_modules{,/**}',
+    '!private{,/**}',
     '!dist{,/**}',
     '!.git{,/**}',
     '!**/.DS_Store'
@@ -16,76 +17,58 @@ function addDefSrcIgnore (srcArr) {
 }
 
 // JavaScript and JSON linter
-gulp.task('lint-js', function () {
-  return gulp
-    .src(addDefSrcIgnore(['**/*.js', '**/*.json']), { dot: true })
-    .pipe($.eslint({ dotfiles: true }))
+function lintJs () {
+  return gulp.src(addDefSrcIgnore(['**/*.js', '**/*.json']), {dot: true})
+    .pipe($.eslint({dotfiles: true}))
     .pipe($.eslint.format())
     .pipe($.eslint.failAfterError());
-});
+}
 
 // HTML linter
-gulp.task('lint-html', function () {
-  return gulp
-    .src(addDefSrcIgnore(['**/*.html']))
-    .pipe($.htmlLint({ htmllintrc: '.htmllintrc.json' }))
-    .pipe($.htmlLint.format())
-    .pipe($.htmlLint.failAfterError());
-});
+function lintHtml () {
+  return gulp.src(addDefSrcIgnore(['**/*.html']))
+    .pipe($.htmllint({config: '.htmllintrc.json', failOnError: true}));
+}
 
 // CSS linter
-gulp.task('lint-css', function () {
-  return gulp.src(addDefSrcIgnore(['**/*.css'])).pipe(
-    $.stylelint({
+function lintCss () {
+  return gulp.src(addDefSrcIgnore(['**/*.css']))
+    .pipe($.stylelint({
       failAfterError: true,
-      reporters: [{ formatter: 'string', console: true }]
-    })
-  );
-});
-
-// Lint all files
-gulp.task('lint', gulp.series('lint-js', 'lint-html', 'lint-css'));
+      reporters: [{formatter: 'string', console: true}]
+    }));
+}
 
 // Remove solutions from exercises
-gulp.task(
-  'remove-solutions',
-  gulp.series('lint', function () {
-    del.sync('dist');
-    return gulp
-      .src(addDefSrcIgnore(['**']), { dot: true })
-      .pipe(
-        $.replace(
-          /^\s*(\/\/|<!--|\/\*)\s*REMOVE-START[\s\S]*?REMOVE-END\s*(\*\/|-->)?\s*$/gm,
-          ''
-        )
-      )
-      .pipe(gulp.dest('dist'));
-  })
+function removeSolutions () {
+  del.sync('dist');
+  return gulp.src(addDefSrcIgnore(['**']), {dot: true})
+    .pipe($.replace(/^\s*(\/\/|<!--|\/\*)\s*REMOVE-START[\s\S]*?REMOVE-END\s*(\*\/|-->)?\s*$/gm, ''))
+    .pipe(gulp.dest('dist'));
+}
+
+// Prepare for distribution to students
+function updateConfigForSlave (done) {
+  let npmConfig = require('./package.json');
+  npmConfig = JSON.stringify(npmConfig, null, 2).replace(/-master/g, '');
+  fs.writeFileSync('dist/package.json', npmConfig);
+  const esLintConfig = require('./.eslintrc.json');
+  esLintConfig.rules['no-undef'] = 'off';
+  esLintConfig.rules['no-unused-vars'] = 'off';
+  fs.writeFileSync('dist/.eslintrc.json', JSON.stringify(esLintConfig, null, 2));
+
+  done();
+}
+
+// Lint all files
+exports.lint = gulp.parallel(
+  lintJs,
+  lintHtml,
+  lintCss
 );
 
 // Prepare for distribution to students
-gulp.task(
-  'dist',
-  gulp.series('remove-solutions', function (done) {
-    function removeMaster (str) {
-      var strArr = str.split('-');
-      strArr[strArr.length - 1] === 'master' && strArr.pop();
-      return strArr.join('-');
-    }
-
-    const npmConfig = require('./package.json');
-    npmConfig.name = removeMaster(npmConfig.name);
-    npmConfig.repository.url = removeMaster(npmConfig.repository.url);
-    npmConfig.scripts['precommit'] = 'gulp lint';
-    fs.writeFileSync('dist/package.json', JSON.stringify(npmConfig, null, 2));
-
-    const esLintConfig = require('./.eslintrc.json');
-    esLintConfig.rules['no-undef'] = 'off';
-    esLintConfig.rules['no-unused-vars'] = 'off';
-    fs.writeFileSync(
-      'dist/.eslintrc.json',
-      JSON.stringify(esLintConfig, null, 2)
-    );
-    done();
-  })
+exports.dist = gulp.series(
+  removeSolutions,
+  updateConfigForSlave
 );
