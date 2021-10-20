d934723826cf740462a792288b8bbf1d2dcb29cb
leaveoscaralone <correawade@gmail.com>
2021-10-20 17:41:38 -0500

    completed basic types

--- stringifier/index.js
+++ stringifier/index.js
@@ -2,7 +2,22 @@
 // Returns a stringified version of input,
 // behaving in exactly the same way as JSON.stringify()
 function stringifier (input) {
-
+  switch(typeof(input)) {
+    case 'number':
+      return `${input}`
+    case 'string':
+      return `"${input}"`
+    case 'object':
+      if (Array.isArray(input)) {
+        return `[${input.toString()}]`
+      } else if (!input) {
+        return 'null'
+      } else {
+      for (let [key, value] of Object.entries(input)) {
+        return `{${stringifier(key)}: ${stringifier(value)}}`
+      }
+    }
+  }
 }
 
 // Allow tests to run on the server (leave at the bottom)
